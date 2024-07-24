b8c280fa94330edaf47c31bf697903e694383a57
Manas Madan <manasmadan08@gmail.com>
2024-07-24 20:50:01 +0530

    Auth Flow Completed

---
+++ src/app/(auth)/no-access/page.tsx
@@ -0,0 +1,11 @@
+import SignOutButton from "@/components/Buttons/SignOutButton";
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      No Access
+      <SignOutButton />
+    </div>
+  );
+}
--- src/middleware.ts
+++ src/middleware.ts
@@ -15,6 +15,34 @@ export async function middleware(req: NextRequest) {
     return NextResponse.redirect(new URL("/signin", req.url));
   }
 
+  if (token.role === "NONE") {
+    if (req.nextUrl.pathname === "/no-access") return NextResponse.next();
+    return NextResponse.redirect(new URL("/no-access", req.url));
+  }
+
+  if (pathname === "/") {
+    switch (token.role) {
+      case "ADMIN":
+        return NextResponse.redirect(new URL("/admin", req.url));
+      case "QC":
+        return NextResponse.redirect(new URL("/qc", req.url));
+      case "SME":
+        return NextResponse.redirect(new URL("/sme", req.url));
+      default:
+        return NextResponse.redirect(new URL("/no-access", req.url));
+    }
+  }
+
+  if (pathname.includes("/admin") && token.role !== "ADMIN") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (pathname.includes("/qc") && token.role !== "QC") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+  if (pathname.includes("/sme") && token.role !== "SME") {
+    return NextResponse.redirect(new URL("/", req.url));
+  }
+
   return NextResponse.next();
 }
 
